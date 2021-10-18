4d9b04d4cfd48d62d1d909efd6d677ced5a7196c
Fantastic-box <91140620+Fantastic-box@users.noreply.github.com>
2021-10-18 17:38:21 +0800

    Complete the questions for grade 3 and 4
    In the file, the user scores were tested by giving random questions in grades 3 and 4.

--- EE308_LAB5.cpp
+++ EE308_LAB5.cpp
@@ -1,22 +1,73 @@
 #include <iostream>
+#include <stdlib.h>
+#include <iomanip>
+#include <time.h>
 using namespace std;
-void Grade12(int);
-void Grade34(int);
-void Grade56(int);
-int main()
-{
-	int grade;
-	int num;
-	cout << "Please enter your grade?"<<endl;
-	cin>>grade;//get grade
-	cout <<"Please enter the number of questions?"<<endl;
-	cin>>num;//get the number of questions
-	if(grade==1|grade==2)Grade12(num);
-	if(grade==3|grade==4)Grade34(num);
-	if(grade==5|grade==6)Grade56(num);
-	return 0;
+
+void Grade34(int q){
+	
+	double result;
+	int n, num1, num2, ans, i;
+    int c = 0, w = 0;
+    
+    for(i = 0;i < q;i++){
+    	n = rand() % 5;
+    	srand((unsigned)time(NULL));
+		num1 = rand();
+    	num2 = rand();
+    	if(n == 1){
+    		num1 = rand();
+    	    num2 = rand();
+    	    result = num1 + num2;
+            cout << num1 << "+" << num2 << "=?" << endl;
+            cin >> ans;
+            if(ans == result){
+				c++;
+			}else{
+				w++;
+			}
+		}else if(n == 2){
+			num1 = rand();
+    	    num2 = rand();
+    	    result = num1 - num2;
+            cout << num1 << "-" << num2 << "=?" << endl;
+            cin >> ans;
+            if(ans == result){
+				c++;
+			}else{
+				w++;
+			}
+		}else if(n == 3){
+			num1 = rand();
+    	    num2 = rand();
+    	    result = num1 * num2;
+            cout << num1 << "*" << num2 << "=?" << endl;
+            cin >> ans;
+            if(ans == result){
+				c++;
+			}else{
+				w++;
+			}
+		}else{
+			num1 = rand();
+    	    num2 = rand();
+    	    result = (num1 / num2)*100/100.00;
+            cout << num1 << "/" << num2 << "=?" << endl;
+            cin >> ans;
+            if(ans == result){
+				c++;
+			}else{
+				w++;
+			}
+		}
+	}
+	
+	result = c * 100 / q;
+		
+	if(c == q){
+		cout << "Output: end! All right, that's great! Your score is 100." << endl;
+	}else{
+		cout << "end! Wrong question, your score is 50" << result << "." << endl;
+	}
 	
 }
-//fun level 1
-//fun level 2
-//fun level 3
